d18f1ea6d6cb9a2ea8316e0fd15e97c8698eeb3d
Leroi Aaron Walwyn <me@aaronwalwyn.com>
2015-11-19 16:47:38 +0000

    Added bootstrap, added d3.js, created header text and button for index.html, added more pages

--- app/views.py
+++ app/views.py
@@ -4,6 +4,22 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
     return render_template("index.html",
-                           title="Home")
+                           title="Home",
+                           g.user.is_authenticated=True)
+@app.route('/about')
+def about():
+    return render_template("about.html",
+                           title="Test")
+
+@app.route('/pathway')
+def pathway():
+    return render_template("pathway.html",
+                           title="Test")
+
+@app.route('/test')
+def test():
+    return render_template("test.html",
+                           title="Test")
