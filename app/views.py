bb3e69ba842945c874dfe20c6a68260e7a3ad3e0
Duncan Cameron <dac95@hotmail.co.uk>
2015-11-19 16:51:47 +0000

    Added basic user view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import app
+from app import app, models
 
 
 @app.route('/')
@@ -7,3 +7,14 @@ from app import app
 def index():
     return render_template("index.html",
                            title="Home")
+
+
+@app.route('/user/<user_id>')
+def user(user_id):
+    print("In user view")
+    user_obj = models.User.query.filter_by(id=user_id).first()
+    print(user_obj)
+    name = user_obj.first_name + " " + user_obj.last_name
+    return render_template("user.html",
+                           user=user_obj,
+                           title=name + "| pathways")
