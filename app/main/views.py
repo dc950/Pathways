5f2feace4bf20a10db9b7b14dccf424d4fc61374
Leroi Aaron Walwyn <me@aaronwalwyn.com>
2016-03-07 17:13:55 +0000

    Fixed edit qualification page

--- app/main/views.py
+++ app/main/views.py
@@ -68,12 +68,13 @@ def edit_qualification(qualification=None):
     all_qual_types = QualificationType.query.all()
     user_subjects = UserQualification.query.join(Qualification, UserQualification.qualifications_id==Qualification.id).all()
     user_qual_types = QualificationType.query.join(Qualification, QualificationType.id==Qualification.qualification_type_id).join(UserQualification, UserQualification.qualifications_id==Qualification.id).filter_by(user_id=current_user.id).all()
+    user_quals = Qualification.query.join(UserQualification, Qualification.id==UserQualification.qualifications_id).all()
     if qualification is None:
         return render_template("edit-qualification.html",
                            title="Edit Qualifications",
                            show_all=True,
                            subjects=user_subjects,
-                           qualifications=user_qual_types)
+                           qualifications=user_quals)
     else:
         form = EditQualificationForm()
         form.qualification_type.data = qualification
